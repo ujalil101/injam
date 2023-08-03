bbdab20926f244b7c520aef8e1f800a94e27ebe9
Usman <ujalil929@gmail.com>
2023-08-03 11:34:13 -0400

    New strucct

--- datacleaning.py
+++ datacleaning.py
@@ -5,27 +5,24 @@ from os import listdir
 
 # get the path/directory
 
-folder_dir = "Test/Fake"
-counter = 1
+folder_dir = "Fake"
+counter = 103
+
 '''
+
 for images in os.listdir(folder_dir):
     if images != ".DS_Store":
-        im = Image.open("{}/{}".format(folder_dir,images))
+       
+        im = Image.open("{}/{}".format(folder_dir,images)).convert("RGB")
         im.save("Image{}.jpg".format(counter),"jpeg")
-
+        counter += 1
 '''
-im = Image.open("/Users/ussie/Desktop/Injam/injam/Test/Fake/Image21.png")
 
-im = im.convert('RGB')
 
- 
-im.save("Image21.jpg",quality=95)
-'''
-folder_dir = "Training/Real"
-counter = 1
+
 for images in os.listdir(folder_dir):
     os.rename("{}/{}".format(folder_dir,images), "Image{}.jpg".format(counter))
     counter+=1
-'''
+
 
     
